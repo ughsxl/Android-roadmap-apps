a4f5feb1d1c1c07c17a4431a0531c1513f8c9a46
ughsxl <101698689+ughsxl@users.noreply.github.com>
2022-06-25 12:57:13 +0300

    Default Project

---
+++ app/src/main/java/android/bignerdranch/roadmapapps/MainActivity.kt
@@ -0,0 +1,16 @@
+package android.bignerdranch.roadmapapps
+
+import android.bignerdranch.roadmapapps.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
